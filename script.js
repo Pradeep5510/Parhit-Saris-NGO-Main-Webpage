c51f29662ccba69fcaab377d9da291af882196db
Pradeep5510 <pradeeppathak5510@gmail.com>
2025-07-03 12:10:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,20 @@
 //     console.log(currMode);
 
 // })
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide() {
+  for (let i = 0; i < totalSlides; i++) {
+    slides[i].classList.remove('active');
+  }
+  slideIndex++;
+  if (slideIndex > totalSlides) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].classList.add('active');
+  setTimeout(showSlide, 2000); // Change slide every 2 seconds
+}
+
+showSlide(); // Start the slideshow
